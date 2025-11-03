ef2979ef826a962dee1814913f5abdc3f494c6e1
Hari-repo <hariharanuk810@gmail.com>
2025-11-03 20:49:28 +0530

    inside a comp memorise the result of double calc so that it doesnt re-render

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { useMemo, useState } from "react"
 
 
 
 function App() {
+ const[num, setNum] = useState(0)
  
-
+ const doubleNum = useMemo(()=>{
+    console.log("Calculating double...");
+    return num*2;
+ },[num] );
   return (
     <>
      
       <h1>Vite + React</h1>
-    
+      <input type="number"
+        onChange={(e)=> setNum(e.target.value)}
+        value={num}
+        />
+        <p>Double : {doubleNum}</p>
     </>
+
   )
 }
 
